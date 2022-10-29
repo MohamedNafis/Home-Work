eaae119ba794b01b5f5c2f329edd7a9c89fb413e
MohamedNafis <mdnafis.mail@gmail.com>
2022-10-29 19:57:34 -0400

    homework

---
+++ src/hw5Q2Constructor/ComputerTest.java
@@ -0,0 +1,9 @@
+package hw5Q2Constructor;
+
+public class ComputerTest {
+	public static void main(String[] args) {
+		Computer computer = new Computer();
+		Computer computer2 = new Computer("Apple", "MacBook Pro", "MacOs Monterey", 2000, 'A', true);
+	}
+
+}
