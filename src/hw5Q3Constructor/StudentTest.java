ae5c7e4c064aab32b7b9f31bc1bc4ce696fe14a5
MohamedNafis <mdnafis.mail@gmail.com>
2022-10-29 20:46:31 -0400

    homework

---
+++ src/hw5Q3Constructor/StudentTest.java
@@ -0,0 +1,9 @@
+package hw5Q3Constructor;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student = new Student();
+		Student student01 = new Student("Mohammed Nafis", 20220424, 'M', true, 4.00f);
+	}
+
+}
