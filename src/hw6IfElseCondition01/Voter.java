4b06600fd8bfdb4ad8e96da5f5962f7471d5148d
MohamedNafis <mdnafis.mail@gmail.com>
2022-11-08 01:41:10 -0500

    homework

---
+++ src/hw6IfElseCondition01/Voter.java
@@ -0,0 +1,20 @@
+package hw6IfElseCondition01;
+
+public class Voter {
+	public static void main(String[] args) {
+		int myAge = 25;
+		int age = 18;
+
+		if (myAge == age) {
+			System.out.println("I am a Voter");
+		} else if (myAge < age) {
+			System.out.println("I am not a Voter");
+		} else if (myAge > age) {
+			System.out.println("I am a Voter");
+		} else {
+			System.out.println("Please add a valid age");
+		}
+
+	}
+
+}
